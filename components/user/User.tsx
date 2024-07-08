2db894715202fdb890bcff8a0456a0ec9da82c7d
Cutean Bogdan Mihai <bogdancutean@proton.me>
2024-07-08 16:27:52 +0300

    Edit - Better User UI

--- components/user/User.tsx
+++ components/user/User.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 interface UserProps {
   name: string;
@@ -6,8 +8,13 @@ interface UserProps {
 
 const User: React.FC<UserProps> = ({ name }) => {
   return (
-    <div className="flex items-center justify-center w-20 h-20 rounded-full bg-blue-500 text-white">
-      {name}
+    <div className="flex flex-col items-center justify-center w-30 h-25 bg-blue-400 shadow-lg rounded-xl p-4 transform transition-all duration-300 hover:scale-105 hover:shadow-2xl">
+      <div className="flex items-center justify-center w-20 h-20 rounded-full bg-blue-500 text-white mb-4">
+        <FontAwesomeIcon icon={faUser} size="2x" />
+      </div>
+      <div className="text-center text-xl font-semibold text-gray-800">
+        {name}
+      </div>
     </div>
   );
 };
